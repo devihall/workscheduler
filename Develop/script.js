46e587c125406ce62a88cd96295eb6ff168feb60
devihall <devis0306@gmail.com>
2022-07-11 13:29:34 -0400

    display currrent date and time

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,15 @@
 
-
+//save and display user input in local storage
 $(".btn").on("click",function(event){
     var formInputEl = $(event.target).siblings("input").val()
     var formIdEl = $(event.target).siblings("input").attr("id");
     console.log ($(event.target).siblings("input").val())
     console.log ($(event.target).siblings("input").attr('id'))
 localStorage.setItem (formIdEl, formInputEl)
+localStorage.getItem (formIdEl, formInputEl)
 })
+
+
+//Display current time
+var now = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
+$("#currentDay").append(now);
